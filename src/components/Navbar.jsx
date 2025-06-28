3e96fac714004352c7fc914edb952842c98a5fe1
Nexoraedge <onlyego1043@gmail.com>
2025-06-28 11:49:04 +0530

    project setup + Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { navLinks } from '../../constants/constant'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div>
+            <a href="#home" className='flex items-center gap-2'>
+                <img src="/images/logo.png" alt="Logo" />
+                <p>Velvet Pour</p>
+            </a>
+            <ul>
+               {navLinks.map((link)=>(
+                    <li key={link.id}>
+                        <a href={`#${link.id}`}>{link.title}</a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
